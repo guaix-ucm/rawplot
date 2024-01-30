9a57a035c835fae8a21c85f9c858a0b07ce69364
Rafael Gonzalez <astrorafael@yahoo.es>
2024-01-30 09:13:55 +0100

    imporved usage of image-pair statistics

--- src/rawplot/ptc/noise_charts.py
+++ src/rawplot/ptc/noise_charts.py
@@ -49,40 +49,31 @@ log = logging.getLogger(__name__)
 # AUXILIARY MAIN FUNCTION
 # -----------------------
 
-def signal_and_total_noise_from(file_list, n_roi, channels, bias):
-    file_list = file_list[::2]
-    N = len(file_list)
-    signal_list = list()
-    noise_list = list()
-    for i, path in enumerate (file_list, start=1):
-        analyzer = ImageStatistics(path, n_roi, channels, bias)
-        analyzer.run()
-        signal_list.append(analyzer.mean())
-        noise_var = analyzer.variance()
-        noise_list.append(noise_var)
-        log.info("[%d/%d] \u03C3\u00b2(total) for image %s = %s", i, N, analyzer.name(), noise_var)
-    return np.stack(signal_list, axis=-1), np.stack(noise_list, axis=-1)
-
-
-def read_and_shot_noise_from(file_list, n_roi, channels, bias):
+def signal_and_noises_from(file_list, n_roi, channels, bias):
     file_pairs = list(zip(file_list, file_list[1:]))[::2]
     N = len(file_pairs)
-    noise_list = list()
+    total_noise_list = list() # Only from the first images of the pairs
+    signal_list = list()      # Only from the first images of the pairs
+    fpn_corrected_noise_list = list()
     for i, (path_a, path_b) in enumerate(file_pairs, start=1):
         analyzer = ImagePairStatistics(path_a, path_b, n_roi, channels, bias)
         analyzer.run()
-        noise_var = analyzer.variance()
-        noise_list.append(noise_var)
-        log.info("[%d/%d] \u03C3\u00b2(sh+rd) for image pair %s = %s",  i, N, analyzer.names(), noise_var)
-    return  np.stack(noise_list, axis=-1)
+        signal = analyzer.mean()
+        total_noise_var = analyzer.variance()
+        fpn_corrected_noise_var = analyzer.adj_pair_variance() # Already corrected by 1/2 factor
+        signal_list.append(signal)
+        total_noise_list.append(total_noise_var)
+        fpn_corrected_noise_list.append(fpn_corrected_noise_var)
+        log.info("[%d/%d] \u03C3\u00b2(total)     for image %s = %.6E", i, N, analyzer.name(), total_noise_var)
+        log.info("[%d/%d] \u03C3\u00b2(total-fpn) for image pair %s = %.6E",  i, N, analyzer.names(), fpn_corrected_noise_var)
+    return  np.stack(signal_list, axis=-1), np.stack(total_noise_list, axis=-1), np.stack(fpn_corrected_noise_list, axis=-1)
 
 
 def signal_and_noise_variances(file_list, n_roi, channels, bias, read_noise):
-    signal, total_noise_var = signal_and_total_noise_from(file_list, n_roi, channels, bias)
-    shot_read_noise_var = read_and_shot_noise_from(file_list, n_roi, channels, bias)
-    fixed_pattern_noise_var = total_noise_var - shot_read_noise_var
-    shot_noise_var = shot_read_noise_var - read_noise**2
-    return signal, total_noise_var, shot_read_noise_var, shot_noise_var, fixed_pattern_noise_var
+    signal, total_noise_var, fpn_corrected_noise_var = signal_and_noises_from(file_list, n_roi, channels, bias)
+    fixed_pattern_noise_var = total_noise_var - fpn_corrected_noise_var
+    shot_noise_var = fpn_corrected_noise_var - read_noise**2
+    return signal, total_noise_var, fpn_corrected_noise_var, shot_noise_var, fixed_pattern_noise_var
 
 
 def plot_read_noise_line(axes, read_noise):
