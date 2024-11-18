0548d91e3a981485e0ec8020ae3c7b0b6af034bc
Rafael Gonzalez <astrorafael@gmail.com>
2024-11-18 13:21:01 +0100

    Parse the TAS CSV file and convert it into data arrays and metadata

--- src/rawplot/tas.py
+++ src/rawplot/tas.py
@@ -13,11 +13,8 @@
 import csv
 import datetime
 
-import math
 import logging
 from typing import Tuple, Any
-from itertools import groupby
-from collections.abc import Iterable
 from argparse import Namespace
 
 # ---------------------
@@ -26,6 +23,7 @@ from argparse import Namespace
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from lica.cli import execute
 from lica.validators import vfile, vsexa
@@ -39,19 +37,6 @@ from ._version import __version__
 # ----------------
 # Module constants
 # ----------------
-
-# Photodiode readings header columns
-WAVELENGTH_CSV_HEADER = "wavelength (nm)"
-CURRENT_CSV_HEADER = "current (A)"
-READ_NOISE_CSV_HEADER = "read noise (A)"
-FREQUENCY_CSV_HEADER = "frequency (Hz)"
-QE_CSV_HEADER = "quantum efficiency"
-RESP_CSV_HEADER = "responsivity"
-
-CUTOFF_FILTERS = [
-    {"label": r"$BG38 \Rightarrow OG570$", "wave": 570, "style": "--"},
-    {"label": r"$OG570\Rightarrow RG830$", "wave": 860, "style": "-."},
-]
 
 # -----------------------
 # Module global variables
@@ -71,53 +56,78 @@ plt.style.use("rawplot.resources.global")
 # -------------------
 
 
+def mycolormap() -> mcolors.LinearSegmentedColormap:
+    """Combine two color maps to produce a new one"""
+    colors2 = plt.cm.viridis_r(np.linspace(0.0, 1, 192))
+    colors1 = plt.cm.YlOrRd_r(np.linspace(0, 1, 64))
+    colors = np.vstack((colors1, colors2))
+    return mcolors.LinearSegmentedColormap.from_list("my_colormap", colors)
+
+
 def mpl_tas_plot_loop(
-    wavelength: np.ndarray,
-    signal: np.ndarray,
-    model: str,
-    sensor: str,
-    responsivity: bool,
-    normalized: bool,
-    filters: Iterable[dict[str, Any]],
+    azimuth: np.ndarray,
+    zenith: np.ndarray,
+    magnitude: np.ndarray,
+    title: str,
+    min_mag: float,
+    max_mag: float,
 ) -> None:
-    fig, axes = plt.subplots(nrows=1, ncols=1)
+    fig, axes = plt.subplots(nrows=1, ncols=1, subplot_kw={"projection": "polar"})
     # fig.suptitle("Corrected Spectral Response plot")
-    axes.set_xlabel("Wavelength [nm]")
-    axes_title = "Responsivity" if responsivity else "Quantum Efficiency"
-    axes.set_title(axes_title)
-    units = "(normalized)" if normalized else ""
-    axes.set_ylabel(f"Signal {units}")
-    axes.plot(wavelength, signal, marker="+", linewidth=1, color="blue", label=f"TESS-W ({sensor})")
-    for filt in filters:
-        axes.axvline(filt["wave"], linestyle=filt["style"], label=filt["label"])
-    axes.grid(True, which="major", color="silver", linestyle="solid")
-    axes.grid(True, which="minor", color="silver", linestyle=(0, (1, 10)))
-    axes.minorticks_on()
-    axes.legend()
+
+    axes.set_theta_zero_location("N")  # Set the north to the north
+    axes.set_theta_direction(-1)
+    axes.set_xticklabels(["N", "NE", "E", "SE", "S", "SW", "W", "NW"], fontdict={"fontsize": 16})
+    axes.tick_params(pad=1.2)
+    axes.set_ylim(0, max(zenith) + 0.0)
+    axes.set_title(title)
+    cmap = mycolormap()
+    cax = axes.scatter(
+        np.radians(azimuth), zenith, c=magnitude, cmap=cmap, vmin=min_mag, vmax=max_mag
+    )
+
+    # Colorbar
+    cb = fig.colorbar(cax, orientation="horizontal", fraction=0.048, ticks=lev_f_ticks, pad=0.08)
+    cb.set_label("Sky Brightness [mag/arcsec$^2$]", fontsize=17)
+    cb.ax.tick_params(labelsize=12)
     plt.show()
 
 
-
 def map_fields(line: list[str]) -> dict[str, Any]:
+    """Each line of TAS CSV is converted to a dictionary with proper data types"""
     result = dict()
-    result["sequence"] = int(line[0])
+    result["sequence"] = int(line[0])  # scanned point sequence number
     result["timestamp"] = datetime.datetime.strptime(line[1] + " " + line[2], "%Y-%m-%d %H:%M:%S")
-    result["temp_sky"] = float(line[3])
-    result["temp_box"] = float(line[4])
-    result["magnitude"] = float(line[5])
-    result["frequency"] = float(line[6])
-    result["altitude"] = float(line[7])
-    result["azimuth"] = float(line[8])
-    result["latitude"] = vsexa(line[9])
-    result["longitude"] = vsexa(line[10])
-    result["msnm"] = float(line[11])
+    result["temp_sky"] = float(line[3])  # degrees Celsius
+    result["temp_box"] = float(line[4])  # degrees Celsius
+    result["magnitude"] = float(line[5])  # mag/arcsec^2
+    result["frequency"] = float(line[6])  # Hz
+    result["altitude"] = float(line[7])  # decimal degrees
+    result["azimuth"] = float(line[8])  # decimal degrees
+    result["latitude"] = vsexa(line[9])  # decimal degrees
+    result["longitude"] = vsexa(line[10])  # decimal degrees
+    result["height"] = float(line[11])  # meters above sea level
     return result
 
 
 def read_tas_file(path: str) -> Tuple[dict[str, Any]]:
     with open(path, "r") as csvfile:
         lines = [line for line in csv.reader(csvfile, delimiter="\t")][1:]
-    return tuple(map(map_fields, lines))
+    temp_data = tuple(map(map_fields, lines))
+    data = dict()
+    metadata = dict()
+    data["sequence"] = np.array([item["sequence"] for item in temp_data])
+    data["zenith"] = np.array([90 - item["altitude"] for item in temp_data])
+    data["azimuth"] = np.array([item["azimuth"] for item in temp_data])
+    data["frequency"] = np.array([item["frequency"] for item in temp_data])
+    data["magnitude"] = np.array([item["magnitude"] for item in temp_data])
+    data["temp_sky"] = np.array([item["temp_sky"] for item in temp_data])
+    metadata["timestamp"] = temp_data[len(temp_data)//2] # Timestamp at mid exposure.
+    metadata["height"] = np.mean(np.array([item["height"] for item in temp_data]))
+    metadata["longitude"] = np.mean(np.array([item["longitude"] for item in temp_data]))
+    metadata["latitude"] = np.mean(np.array([item["latitude"] for item in temp_data]))
+    metadata["temp_box"] = np.mean(np.array([item["temp_box"] for item in temp_data]))
+    return data, metadata
 
 
 # -----------------------
@@ -126,8 +136,8 @@ def read_tas_file(path: str) -> Tuple[dict[str, Any]]:
 
 
 def tas(args: Namespace):
-    result = read_tas_file(args.input_file)
-    log.info(result)
+    data, metadata = read_tas_file(args.input_file)
+    log.info(data)
 
 
 # ===================================
