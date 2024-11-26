2fd26100f3d25267605352498238790b6da286fb
Rafael Gonzalez <astrorafael@yahoo.es>
2024-11-26 07:46:01 +0100

    still a draft

--- src/rawplot/tas.py
+++ src/rawplot/tas.py
@@ -37,6 +37,8 @@ import astropy.timeseries
 import astropy.units as u
 from astropy.timeseries import TimeSeries
 
+from scipy.interpolate import griddata
+
 import pytz
 from timezonefinder import TimezoneFinder
 
@@ -69,9 +71,9 @@ plt.style.use("rawplot.resources.global")
 
 
 def mycolormap() -> mcolors.LinearSegmentedColormap:
-    """Combine two color maps to produce a new one"""
-    colors2 = plt.cm.viridis_r(np.linspace(0.0, 1, 192))
-    colors1 = plt.cm.YlOrRd_r(np.linspace(0, 1, 64))
+    """Combine two color maps to produce a 256 color palette one"""
+    colors2 = plt.cm.viridis_r(np.linspace(0.0, 1, 192)) # Reverse "viridis" color map
+    colors1 = plt.cm.YlOrRd_r(np.linspace(0, 1, 64)) # Reverse "Yellow Orange Red" color map
     colors = np.vstack((colors1, colors2))
     return mcolors.LinearSegmentedColormap.from_list("my_colormap", colors)
 
@@ -79,7 +81,9 @@ def mycolormap() -> mcolors.LinearSegmentedColormap:
 def mpl_tas_plot_loop(
     azimuth: np.ndarray,
     zenith: np.ndarray,
-    magnitude: np.ndarray,
+    original_magnitude: np.ndarray,
+    interpolated_magnitude: np.ndarray,
+    mesh: Tuple[np.ndarray, np.ndarray],
     title: str,
     min_mag: float,
     max_mag: float,
@@ -91,13 +95,21 @@ def mpl_tas_plot_loop(
     axes.set_theta_direction(-1)
     axes.set_xticklabels(["N", "NE", "E", "SE", "S", "SW", "W", "NW"], fontdict={"fontsize": 16})
     axes.tick_params(pad=1.2)
-    max_y = np.max(zenith) / (1 *u.deg) # make dimesionless, plain Python scalar
+    max_y = np.max(zenith) / (1 * u.deg) # make a dimesionless, plain Python scalar
     axes.set_ylim(0, max_y + 0.0)
     axes.set_title(title)
-    cmap = mycolormap()
-    cax = axes.scatter(
-        np.radians(azimuth), zenith, c=magnitude, cmap=cmap, vmin=min_mag, vmax=max_mag
-    )
+
+    if False:
+        # Display the TAS readings as colors in a polar plot whose radius is the zenithal distance
+        cax = axes.scatter(
+            np.radians(azimuth), zenith, c=magnitude, cmap=mycolormap(), vmin=min_mag, vmax=max_mag
+        )
+    else:
+        cax = axes.contourf(self.i_azimuths, self.i_zeniths, self.i_magnitudes , self.lev_f, cmap=cmap,
+                              vmin=min_mag, vmax=max_mag)
+        # Display the TAS readings simply as red dot in a polar plot whose radius is the zenithal distance
+        cax = axes.scatter(
+            np.radians(azimuth), zenith, c="red", s=16, zorder=2)
 
     # Colorbar
     m_step_1 = 0.2
@@ -147,7 +159,7 @@ def tas_metadata(rows: Tuple[dict[str, Any]]) -> dict[str, Any]:
     return metadata
 
 
-def to_astropy(row):
+def to_astropy(row: dict[str, Any]) -> dict[str, Any]:
     """Use specialized Astropy Types for some columns"""
     row["longitude"] = astropy.coordinates.Longitude(row["longitude"] * u.deg)
     row["latitude"] = astropy.coordinates.Latitude(row["latitude"] * u.deg)
@@ -160,7 +172,7 @@ def to_astropy(row):
 def read_tas_file(path: str) -> Tuple[TimeSeries, dict[str, Any]]:
     with open(path, "r") as csvfile:
         lines = [line for line in csv.reader(csvfile, delimiter="\t")][1:]
-    timezone = get_timezone(lines[0])  # get the timezone from the first line
+    timezone = get_timezone(lines[0])  # get the timezone using the first line readings 
     tzinfo = pytz.timezone(timezone)
     map_fields = functools.partial(map_fields2, tzinfo)
     # Low level decoding to Python standard datatups
@@ -201,6 +213,37 @@ def read_tas_file(path: str) -> Tuple[TimeSeries, dict[str, Any]]:
     )
     return table, metadata
 
+def interpolate(data: TimeSeries) -> np.ndarray:
+    
+    max_zenith =  np.max(data["zenith"]) / (1 * u.deg)
+    zen_r = np.arange(0, max_zenith+1)  # coordiantes r values in zenithal (r) direction
+    azi_theta = np.arange(0, 361)  # coordiantes values in azimuth (theta) direction
+    ZEN_R, AZI_THETA = np.meshgrid(zen_r, azi_theta)  # coordinated zen and azi (for vectorized computations) INTERPOLATION grid
+    log.info("ZEN_R shape: %s",ZEN_R.shape)
+    log.info("AZI_THETA shape: %s",AZI_THETA.shape)
+
+    # '''Create periodic boundary conditions for interpolation'''
+    dfa = data.copy()
+    dfa['azimuth'] = data['azimuth'] +  360 * u.deg
+    dfb = data.copy()
+    dfb['azimuth'] = data['azimuth'] - 360 * u.deg
+    periodic_data = np.vstack((data, dfa, dfb))
+
+    magnitudes = data["magnitude"].ravel()
+    zenith = data["zenith"].ravel()
+    azimuth = data["azimuth"].ravel()
+
+    #log.info("periodic_data shape: %s",data.shape)
+    log.info("magnitudes shape: %s",magnitudes.shape)
+    log.info("zenith shape: %s",zenith.shape)
+    log.info("azimuth shape: %s",azimuth.shape)
+
+    
+    #r, theta = np.meshgrid(zen, np.radians(azi))  # coordinated zen and azi (for vectorized computations)
+    # magnitudes
+    mag_interp = griddata((zenith, azimuth), magnitudes, (ZEN_R, AZI_THETA), method = 'cubic')  # Interpolated values (magnitudes) for the grid points
+    log.info("interpolated magnitudes shape: %s",mag_interp.shape)
+    return  mag_interp, (ZEN_R, AZI_THETA)
 
 # -----------------------
 # AUXILIARY MAIN FUNCTION
@@ -214,11 +257,14 @@ def tas(args: Namespace):
 
     log.info(data.info)
     log.info(metadata)
+    mag_interp, mesh = interpolate(data)
 
     mpl_tas_plot_loop(
         azimuth=data["azimuth"],
         zenith=data["zenith"],
-        magnitude=data["magnitude"],
+        original_magnitude=data["magnitude"],
+        interpolated_magnitude=mag_interp,
+        mesh=mesh,
         title="My Polar Plot",
         min_mag=17,
         max_mag=22,
