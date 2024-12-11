17a837fb1b63b9e93e00230415f03d442a880a0b
Rafael Gonzalez <astrorafael@yahoo.es>
2024-12-11 13:10:26 +0100

    photoiode readings plot working

--- src/rawplot/spectral.py
+++ src/rawplot/spectral.py
@@ -10,9 +10,10 @@
 # System wide imports
 # -------------------
 
-import re
+from enum import IntEnum
 import math
 import logging
+from argparse import ArgumentParser
 
 # ---------------------
 # Thrid-party libraries
@@ -21,10 +22,17 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
+import astropy.io.ascii
+import astropy.units as u
+from astropy.table import Table
+
+from lica import StrEnum
 from lica.cli import execute
 from lica.validators import vdir, vfile, vfloat, vfloat01, vflopath
 from lica.raw.analyzer.image import ImageStatistics
 from lica.csv import read_csv
+import lica.photodiode
+from lica.photodiode import PhotodiodeModel, COL, BENCH
 
 # ------------------------
 # Own modules and packages
@@ -32,18 +40,36 @@ from lica.csv import read_csv
 
 from ._version import __version__
 from .util.common import common_list_info, make_plot_title_from, export_spectra_to_csv
-from .photodiode import photodiode_load, OSI_PHOTODIODE, HAMAMATSU_PHOTODIODE
+
+# -----------------
+# Additiona classes
+# -----------------
+
+
+class TBCOL(StrEnum):
+    """Additiona columns names for data produced by Scan.exe or TestBench"""
+
+    INDEX = "Index"  # Index number 1, 2, etc produced in the CSV file
+    CURRENT = "Electrical Current"  #
+    READ_NOISE = "Read Noise"
+
+
+class PhDOption(IntEnum):
+    """Photodiode plot options"""
+
+    RAW = 1
+    NORM = 2
+    SNR = 3
+
 
 # ----------------
 # Module constants
 # ----------------
 
-WAVELENGTH_REG_EXP = re.compile(r"(\w+)_(\d+)nm_g(\d+)_(\d+)_(\d+)_(\w+).jpg")
-
-# Photodiode readings header columns
-WAVELENGTH_CSV_HEADER = "wavelength (nm)"
-CURRENT_CSV_HEADER = "current (A)"
-READ_NOISE_CSV_HEADER = "read noise (A)"
+MONOCROMATOR_FILTERS_LABELS = (
+    {"label": r"$BG38 \Rightarrow OG570$", "wave": 570, "style": "--"},
+    {"label": r"$OG570\Rightarrow RG830$", "wave": 860, "style": "-."},
+)
 
 # -----------------------
 # Module global variables
@@ -61,6 +87,21 @@ plt.style.use("rawplot.resources.global")
 # ------------------
 # Auxiliary fnctions
 # ------------------
+
+
+def read_manual_csv(path: str) -> Table:
+    """Load CSV files produced by manually copying LICA TestBench.exe into a CSV file"""
+    table = astropy.io.ascii.read(
+        path,
+        delimiter=";",
+        data_start=1,
+        names=(COL.WAVE, TBCOL.CURRENT, TBCOL.READ_NOISE),
+        converters={COL.WAVE: np.float64, TBCOL.CURRENT: np.float64, TBCOL.READ_NOISE: np.float64},
+    )
+    table[COL.WAVE] = np.round(table[COL.WAVE], decimals=0) * u.nm
+    table[TBCOL.CURRENT] = np.abs(table[TBCOL.CURRENT]) * u.A
+    table[TBCOL.READ_NOISE] = table[TBCOL.READ_NOISE] * u.A
+    return table
 
 
 def mpl_photodiode_plot_loop(title, x, y, xtitle, ytitle, **kwargs):
@@ -141,30 +182,6 @@ def signal_from(file_list, n_roi, channels, bias, dark, every=2):
     return np.stack(exptime_list, axis=-1), np.stack(signal_list, axis=-1)
 
 
-def get_used_wavelengths(file_list, channels):
-    M = len(channels)
-    data = list()
-    for file in file_list:
-        matchobj = WAVELENGTH_REG_EXP.search(file)
-        if matchobj:
-            item = {
-                key: matchobj.group(i)
-                for i, key in enumerate(
-                    ("tag", "wave", "gain", "seq", "exptime", "filter"), start=1
-                )
-            }
-            item["wave"] = int(item["wave"])
-            item["gain"] = int(item["gain"])
-            item["seq"] = int(item["seq"])
-            item["exptime"] = int(item["exptime"])
-            data.append(item)
-    log.info("Matched %d files", len(data))
-    result = np.array([item["wave"] for item in data])
-    result = np.tile(result, M).reshape(M, len(data))
-    log.info("Wavelengthss array shape is %s", result.shape)
-    return result
-
-
 def photodiode_readings_to_arrays(csv_path):
     response = read_csv(csv_path)
     wavelength = np.array([int(entry[WAVELENGTH_CSV_HEADER]) for entry in response])
@@ -174,12 +191,49 @@ def photodiode_readings_to_arrays(csv_path):
     return wavelength, current, read_noise
 
 
-# -----------------------
-# AUXILIARY MAIN FUNCTION
-# -----------------------
+# ---------------------
+# Exported, non-CLI API
+# ---------------------
 
 
-def raw_spectrum(args):
+def photodiode_spectrum(path: str, model: str, option: PhDOption, resolution: int) -> None:
+    readings = read_manual_csv(path)
+    reference = lica.photodiode.load(model=model, resolution=resolution)
+    if option == PhDOption.RAW:
+        title = "Raw Photodiode Signal vs Wavelength"
+        y = readings[TBCOL.CURRENT]
+        ytitle = "Current [A]"
+        ylogscale = False
+    elif option == PhDOption.NORM:
+        title = "Raw Photodiode Signal vs Wavelength"
+        y = readings[TBCOL.CURRENT] / np.max(readings[TBCOL.CURRENT])
+        ytitle = "Current (normalized)"
+        ylogscale = False
+    else:
+        title = "Photodiode SNR vs Wavelength"
+        y = readings[TBCOL.CURRENT] / readings[TBCOL.READ_NOISE]
+        ytitle = "SNR"
+        ylogscale = True
+    mpl_photodiode_plot_loop(
+        title=title,
+        xtitle="Wavelength [nm]",
+        ytitle=ytitle,
+        x=readings[COL.WAVE],
+        y=y,
+        # Optional arguments to be handled by the plotting function
+        ylogscale=ylogscale,
+        filters=MONOCROMATOR_FILTERS_LABELS,  # where filters were changesd
+        qe=reference[COL.QE] if option == PhDOption.NORM else None,
+        photodiode=model if option == PhDOption.NORM else None,
+    )
+
+
+# ===================================
+# Command Line Interface Entry Points
+# ===================================
+
+
+def cli_raw_spectrum(args):
     log.info(" === DRAFT SPECTRAL RESPONSE PLOT === ")
     file_list, roi, n_roi, channels, metadata = common_list_info(args)
     title = make_plot_title_from("Draft Spectral Response plot", metadata, roi)
@@ -202,7 +256,7 @@ def raw_spectrum(args):
     )
 
 
-def corrected_spectrum(args):
+def cli_corrected_spectrum(args):
     log.info(" === COMPLETE SPECTRAL RESPONSE PLOT === ")
     responsivity, qe = photodiode_load(args.model, args.resolution)
     log.info(
@@ -244,227 +298,145 @@ def corrected_spectrum(args):
     )
 
 
-def photodiode_spectrum(args):
+def cli_photodiode_spectrum(args):
     log.info(" === PHOTODIODE SPECTRAL RESPONSET PLOT === ")
-    wavelength, current, read_noise = photodiode_readings_to_arrays(args.csv_file)
-    responsivity, qe = photodiode_load(args.model, args.resolution)
-    qe = np.array(
-        [qe[w] for w in wavelength]
-    )  # Only use those wavelenghts actually used in the CSV sequence
     if args.raw_readings:
-        title = "Raw Photodiode Signal vs Wavelength"
-        y = current
-        ytitle = "Current [A]"
-        ylogscale = False
+        option = PhDOption.RAW
     elif args.normalized:
-        title = "Raw Photodiode Signal vs Wavelength"
-        y = current / np.max(current)
-        ytitle = "Current (normalized)"
-        ylogscale = False
+        option = PhDOption.NORM
     else:
-        title = "Photodiode SNR vs Wavelength"
-        y = current / read_noise
-        ytitle = "SNR"
-        ylogscale = True
-    mpl_photodiode_plot_loop(
-        title=title,
-        xtitle="Wavelength [nm]",
-        ytitle=ytitle,
-        x=wavelength,
-        y=y,
-        # Optional arguments to be handled by the plotting function
-        ylogscale=ylogscale,
-        filters=[
-            {"label": r"$BG38 \Rightarrow OG570$", "wave": 570, "style": "--"},
-            {"label": r"$OG570\Rightarrow RG830$", "wave": 860, "style": "-."},
-        ],  # where filters were changesd
-        qe=qe if args.normalized else None,
-        photodiode=args.model if args.normalized else None,
+        option = PhDOption.SNR
+    photodiode_spectrum(args.photodiode_file, args.model, option, args.resolution)
+
+
+def cli_spectral(args):
+    args.func(args)
+
+
+def prs_aux_files() -> ArgumentParser:
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-bi",
+        "--bias",
+        type=vflopath,
+        help="Bias, either a single value for all channels or else a 3D FITS cube file (default: %(default)s)",
     )
+    parser.add_argument(
+        "-dk",
+        "--dark",
+        type=vfloat,
+        help="Dark count rate in DN/sec. (default: %(default)s)",
+    )
+    return parser
 
 
-COMMAND_TABLE = {
-    "raw": raw_spectrum,
-    "corrected": corrected_spectrum,
-    "photodiode": photodiode_spectrum,
-}
+def prs_images() -> ArgumentParser:
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-i",
+        "--input-dir",
+        type=vdir,
+        required=True,
+        help="Input directory with RAW files",
+    )
+    parser.add_argument(
+        "-f",
+        "--image-filter",
+        type=str,
+        required=True,
+        help="Images filter, glob-style (i.e. flat*, dark*)",
+    )
+    parser.add_argument(
+        "-x",
+        "--x0",
+        type=vfloat01,
+        help="Normalized ROI start point, x0 coordinate [0..1]",
+    )
+    parser.add_argument(
+        "-y",
+        "--y0",
+        type=vfloat01,
+        help="Normalized ROI start point, y0 coordinate [0..1]",
+    )
+    parser.add_argument(
+        "-wi",
+        "--width",
+        type=vfloat01,
+        default=1.0,
+        help="Normalized ROI width [0..1] (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-he",
+        "--height",
+        type=vfloat01,
+        default=1.0,
+        help="Normalized ROI height [0..1] (default: %(default)s) ",
+    )
+    parser.add_argument(
+        "-c",
+        "--channels",
+        default=("R", "Gr", "Gb", "B"),
+        nargs="+",
+        choices=("R", "Gr", "Gb", "G", "B"),
+        help="color plane to plot. G is the average of G1 & G2. (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--every",
+        type=int,
+        metavar="<N>",
+        default=1,
+        help="pick every n `file after sorting",
+    )
+    return parser
 
 
-def spectral(args):
-    command = args.command
-    func = COMMAND_TABLE[command]
-    func(args)
-
-
-# ===================================
-# MAIN ENTRY POINT SPECIFIC ARGUMENTS
-# ===================================
+def prs_photod() -> ArgumentParser:
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-ph",
+        "--photodiode-file",
+        type=vfile,
+        required=True,
+        help="CSV file with photdiode readings",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        default=PhotodiodeModel.OSI,
+        choices=[p for p in PhotodiodeModel],
+        help="Photodiode model. (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-r",
+        "--resolution",
+        type=int,
+        default=5,
+        choices=tuple(range(1, 11)),
+        help="Wavelength resolution (nm). (default: %(default)s nm)",
+    )
+    return parser
 
 
 def add_args(parser):
     subparser = parser.add_subparsers(dest="command")
 
-    parser_raw = subparser.add_parser("raw", help="Raw spectrum")
-    parser_corr = subparser.add_parser("corrected", help="Correced spectrum")
-    parser_diode = subparser.add_parser("photodiode", help="Photodiode readings")
+    parser_raw = subparser.add_parser(
+        "raw", parents=[prs_images(), prs_aux_files()], help="Raw spectrum"
+    )
+    parser_raw.set_defaults(func=cli_raw_spectrum)
+    parser_corr = subparser.add_parser(
+        "corrected",
+        parents=[prs_images(), prs_aux_files(), prs_photod()],
+        help="Corrected spectrum",
+    )
+    parser_corr.set_defaults(func=cli_corrected_spectrum)
+    parser_diode = subparser.add_parser(
+        "photodiode", parents=[prs_photod()], help="Photodiode readings"
+    )
+    parser_diode.set_defaults(func=cli_photodiode_spectrum)
+
     # ---------------------------------------------------------------------------------------------------------------
-    parser_raw.add_argument(
-        "-i",
-        "--input-dir",
-        type=vdir,
-        required=True,
-        help="Input directory with RAW files",
-    )
-    parser_raw.add_argument(
-        "-f",
-        "--image-filter",
-        type=str,
-        required=True,
-        help="Images filter, glob-style (i.e. flat*, dark*)",
-    )
-    parser_raw.add_argument(
-        "-x",
-        "--x0",
-        type=vfloat01,
-        help="Normalized ROI start point, x0 coordinate [0..1]",
-    )
-    parser_raw.add_argument(
-        "-y",
-        "--y0",
-        type=vfloat01,
-        help="Normalized ROI start point, y0 coordinate [0..1]",
-    )
-    parser_raw.add_argument(
-        "-wi",
-        "--width",
-        type=vfloat01,
-        default=1.0,
-        help="Normalized ROI width [0..1] (default: %(default)s)",
-    )
-    parser_raw.add_argument(
-        "-he",
-        "--height",
-        type=vfloat01,
-        default=1.0,
-        help="Normalized ROI height [0..1] (default: %(default)s) ",
-    )
-    parser_raw.add_argument(
-        "-c",
-        "--channels",
-        default=("R", "Gr", "Gb", "B"),
-        nargs="+",
-        choices=("R", "Gr", "Gb", "G", "B"),
-        help="color plane to plot. G is the average of G1 & G2. (default: %(default)s)",
-    )
-    parser_raw.add_argument(
-        "--every",
-        type=int,
-        metavar="<N>",
-        default=1,
-        help="pick every n `file after sorting",
-    )
-    parser_raw.add_argument(
-        "-bi",
-        "--bias",
-        type=vflopath,
-        help="Bias, either a single value for all channels or else a 3D FITS cube file (default: %(default)s)",
-    )
-    parser_raw.add_argument(
-        "-dk",
-        "--dark",
-        type=vfloat,
-        help="Dark count rate in DN/sec. (default: %(default)s)",
-    )
-    # ---------------------------------------------------------------------------------------------------------------
-    parser_corr.add_argument(
-        "-i",
-        "--input-dir",
-        type=vdir,
-        required=True,
-        help="Input directory with RAW files",
-    )
-    parser_corr.add_argument(
-        "-f",
-        "--image-filter",
-        type=str,
-        required=True,
-        help="Images filter, glob-style (i.e. flat*, dark*)",
-    )
-    parser_corr.add_argument(
-        "-x",
-        "--x0",
-        type=vfloat01,
-        help="Normalized ROI start point, x0 coordinate [0..1]",
-    )
-    parser_corr.add_argument(
-        "-y",
-        "--y0",
-        type=vfloat01,
-        help="Normalized ROI start point, y0 coordinate [0..1]",
-    )
-    parser_corr.add_argument(
-        "-wi",
-        "--width",
-        type=vfloat01,
-        default=1.0,
-        help="Normalized ROI width [0..1] (default: %(default)s)",
-    )
-    parser_corr.add_argument(
-        "-he",
-        "--height",
-        type=vfloat01,
-        default=1.0,
-        help="Normalized ROI height [0..1] (default: %(default)s) ",
-    )
-    parser_corr.add_argument(
-        "-c",
-        "--channels",
-        default=("R", "Gr", "Gb", "B"),
-        nargs="+",
-        choices=("R", "Gr", "Gb", "G", "B"),
-        help="color plane to plot. G is the average of G1 & G2. (default: %(default)s)",
-    )
-    parser_corr.add_argument(
-        "--every",
-        type=int,
-        metavar="<N>",
-        default=1,
-        help="pick every n `file after sorting",
-    )
-    parser_corr.add_argument(
-        "-bi",
-        "--bias",
-        type=vflopath,
-        help="Bias, either a single value for all channels or else a 3D FITS cube file (default: %(default)s)",
-    )
-    parser_corr.add_argument(
-        "-dk",
-        "--dark",
-        type=vfloat,
-        help="Dark count rate in DN/sec. (default: %(default)s)",
-    )
-    parser_corr.add_argument(
-        "-cv",
-        "--csv-file",
-        type=vfile,
-        required=True,
-        help="CSV file with photdiode readings",
-    )
-    parser_corr.add_argument(
-        "-m",
-        "--model",
-        default=OSI_PHOTODIODE,
-        choices=(HAMAMATSU_PHOTODIODE, OSI_PHOTODIODE),
-        help="Photodiode model. (default: %(default)s)",
-    )
-    parser_corr.add_argument(
-        "-r",
-        "--resolution",
-        type=int,
-        default=5,
-        choices=(1, 5),
-        help="Wavelength resolution (nm). (default: %(default)s nm)",
-    )
+
     parser_corr.add_argument(
         "--export",
         type=str,
@@ -486,13 +458,6 @@ def add_args(parser):
         help="Wavelength is last column in exported file",
     )
     # ---------------------------------------------------------------------------------------------------------------
-    parser_diode.add_argument(
-        "-cv",
-        "--csv-file",
-        type=vfile,
-        required=True,
-        help="CSV file with photdiode readings",
-    )
     dioex1 = parser_diode.add_mutually_exclusive_group(required=True)
     dioex1.add_argument(
         "-w",
@@ -508,24 +473,9 @@ def add_args(parser):
     )
     dioex1.add_argument(
         "-s",
-        "--signal-to-noise",
+        "--snr",
         action="store_true",
         help="Plot Raw Signal to Noise Ratio",
-    )
-    parser_diode.add_argument(
-        "-m",
-        "--model",
-        default=OSI_PHOTODIODE,
-        choices=(HAMAMATSU_PHOTODIODE, OSI_PHOTODIODE),
-        help="Photodiode model. (default: %(default)s)",
-    )
-    parser_diode.add_argument(
-        "-r",
-        "--resolution",
-        type=int,
-        default=5,
-        choices=(1, 5),
-        help="Wavelength resolution (nm). (default: %(default)s nm)",
     )
 
 
@@ -536,7 +486,7 @@ def add_args(parser):
 
 def main():
     execute(
-        main_func=spectral,
+        main_func=cli_spectral,
         add_args_func=add_args,
         name=__name__,
         version=__version__,
